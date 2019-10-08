97d257d183f48a39220971edc27c2260af476491
danileao <dani.leao89@gmail.com>
2019-10-08 14:48:25 -0300

    Cors

--- src/server.js
+++ src/server.js
@@ -12,15 +12,7 @@ const io = socketio(server);
 
 const connectedUsers = {};
 
-app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors({ origin: "*" }));
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0-ieixk.mongodb.net/omnistack9?retryWrites=true&w=majority",
@@ -47,4 +39,6 @@ app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-server.listen(3333);
+const PORT = process.env.PORT || 3333;
+
+server.listen(PORT);
